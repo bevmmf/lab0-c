dc648f1f9bf30e3d69d6bf0b4454206d493667f1
bevmmf <bevis30401@gmail.com>
2025-02-09 19:59:54 +0800

    Finish q_new() and verify it properly
    This commit make me clarify the project is to implement
    a queue with doubly linked list and the simplest function
    is q_new to create an empty queue.

--- queue.c
+++ queue.c
@@ -14,7 +14,11 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *dummy = malloc(sizeof(struct list_head));
+    if (!dummy)
+        return NULL;
+    INIT_LIST_HEAD(dummy);
+    return dummy;
 }
 
 /* Free all storage used by queue */
